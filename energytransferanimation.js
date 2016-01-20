39fc35232940a9f1ac4720d80ae46239b48e40f5
Richard <r.stones@ucl.ac.uk>
2016-01-20 22:42:26 +0000

    Adding file for main sketch as well as dependencies and html file for testing

---
+++ energytransferanimation.js
@@ -0,0 +1,14 @@
+var energyTransferAnimation = new p5(function(sketch) {
+	
+	sketch.setup = function() {
+		sketch.createCanvas(400,200);
+		// generate Hamiltonian and quantum jump data in new worker thread
+		// loading screen
+		// draw three environments, pigment complex, controls etc
+	};
+
+	sketch.draw = function() {
+		
+	};
+
+}, 'energy-transfer-animation');
