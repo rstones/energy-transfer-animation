78892ae07c0990e139390cd50e5dab2c6abdb90c
Richard <r.stones@ucl.ac.uk>
2016-03-01 23:12:49 +0000

    Environments placeholders and chromophores now display, and basic drag and drop functionality implemented

--- energytransferanimation.js
+++ energytransferanimation.js
@@ -6,11 +6,14 @@ var energyTransferAnimation = new p5(function(sketch) {
 	sketch.timestep = 0.001;
 	sketch.totalTime = 10.0;
 	sketch.qJump = 0//new QJump(sketch.initState, sketch.hamiltonian, sketch.jumpRates, sketch.timestep, sketch.totalTime);
+	
+	sketch.canvasWidth = 1500;
+	sketch.canvasHeight = 600;
 
 	sketch.envCouplingVals = [0, 10, 100];
 	sketch.environments = [];
 	sketch.currentEnvironment = 0;
-	sketch.envPositions = [sketch.createVector(300,300),sketch.createVector(600,300),sketch.createVector(900,300)]; // define centre positions
+	sketch.envPositions = [sketch.createVector(300,300),sketch.createVector(750,300),sketch.createVector(1200,300)]; // define centre positions
 	sketch.chromophoreRelativePositions = [sketch.createVector(0,0),sketch.createVector(-100,100),sketch.createVector(20,120),sketch.createVector(100,100),sketch.createVector(-50,150),
 						sketch.createVector(75,200),sketch.createVector(-25,300)];
 	sketch.complex = null;
@@ -42,10 +45,19 @@ var energyTransferAnimation = new p5(function(sketch) {
 		},
 		// anchorPos is position to translate origin to before drawing Chromophore
 		// the anchorPos will be either associated with Environment pos or mouse pos if Complex is currently being dragged
-		display: function() {
+		display: function(beingDragged) {
 			// draw Chromophore (start as a circle until the rest is working)
 			// need to work out best way to define position since we are going to be dragging the whole Complex around
 			// fill with opacity scaled by population
+			if (beingDragged) {
+				sketch.stroke(125);
+				sketch.fill(125);
+				sketch.ellipse(this.relativePos.x+3, this.relativePos.y+3, this.size, this.size);
+
+			}
+			sketch.stroke(0);
+			sketch.fill(200);
+			sketch.ellipse(this.relativePos.x, this.relativePos.y, this.size, this.size);
 			sketch.fill(255,0,0,255*this.population);
 			sketch.ellipse(this.relativePos.x, this.relativePos.y, this.size, this.size);
 		}
@@ -57,8 +69,10 @@ var energyTransferAnimation = new p5(function(sketch) {
 		
 		this.chromophores = [];
 		this.relativePositions = relativePositions;
+		this.anchorOffset = -150;
+		this.beingDragged = false;
 		for (var i = 0; i < this.relativePositions.length; i++) {
-			this.chromophores.push(new sketch.Chromophore(initPops[i]));
+			this.chromophores.push(new sketch.Chromophore(this.relativePositions[i], initPops[i]));
 		}
 	};
 	
@@ -69,28 +83,47 @@ var energyTransferAnimation = new p5(function(sketch) {
 				this.chromophores[i].updatePopulation(pops[i]);
 			}
 		},
-		display: function(anchorPos) {
-			// translate origin to currently selected environment
-			// call display functions common to all chromophores so as to avoid calling multiple times in every Chromophore.display
-			sketch.translate(anchorPos[0], anchorPos[1]);
+		display: function(envAnchorPos) {
+			if (this.beingDragged) {
+				anchorPos = sketch.createVector(sketch.mouseX, sketch.mouseY);
+			} else {
+				anchorPos = envAnchorPos;
+			}
+			sketch.translate(anchorPos.x, anchorPos.y+this.anchorOffset);
 			sketch.strokeWeight(2);
 			sketch.stroke(0);
 			for (var i = 0; i < this.chromophores.length; i++) {
-				this.chromophores[i].display();
+				this.chromophores[i].display(this.beingDragged);
 			}
-			// any extra bits to display?
-			// translate back? need to find out how translate works...
+			// check if mouseOver then highlight if so?
+			sketch.translate(-anchorPos.x, -(anchorPos.y+this.anchorOffset));
+		},
+		mouseOver: function() {
+			// if mouse over chromophore area (do i need to save current position and dimensions?)
+			// return true
+			// also need to make it work for touch!
+			return true;
+		},
+		drag: function() {
+			this.beingDragged = true;
+			// if mouse down and mouseOver are true then set beingDragged to true and centre position of Complex follows mouse (and add shadow effect)
+		},
+		drop: function() {
+			// if beingDragged is true while mouse released find nearest environment and send it there (with a nice spring effect?)
+			// then set beingDragged to false
+			this.beingDragged = false;
 		}
 	};
 	
 	// constructor function for Environment object prototype
 	// a single parameter should quantify system-environment coupling for a particular instance, to control both quantum jump calculations
 	// and the representation of the environment for the animation (maybe some spring/coiled stretchy cord simulations?)
-	sketch.Environment = function(pos) {
-
+	sketch.Environment = function(coupling, pos) {
+		
+		this.coupling = coupling;
 		this.pos = pos;
 		this.anchorPos = this.pos; // + offset ?
-		this.width = 300;
+		this.width = 400;
 		this.height = 500;
 
 	};
@@ -98,6 +131,7 @@ var energyTransferAnimation = new p5(function(sketch) {
 	sketch.Environment.prototype = {
 		constructor: sketch.Environment,
 		display: function() {
+			sketch.stroke(0);
 			sketch.rect(this.pos.x, this.pos.y, this.width, this.height); 
 		}
 	};
@@ -118,9 +152,10 @@ var energyTransferAnimation = new p5(function(sketch) {
 
 
 	sketch.setup = function() {
-		sketch.createCanvas(1000,1000);
+		sketch.createCanvas(sketch.canvasWidth, sketch.canvasHeight);
 		sketch.smooth();
 		sketch.rectMode(sketch.CENTER);
+		sketch.background(200);
 		// generate Hamiltonian and quantum jump data in new worker thread
 		// create qjump object to do the calculations
 		// loading screen
@@ -136,6 +171,7 @@ var energyTransferAnimation = new p5(function(sketch) {
 	};
 
 	sketch.draw = function() {
+		sketch.background(200);
 		//var calculationFinished = sketch.qJump.nextTimestep(); // need to check whether calculation has terminated each time so we can reset
 		//sketch.complex.updatePopulations(qJump.populations());
 		for (var i = 0; i < sketch.environments.length; i++) {
@@ -146,6 +182,18 @@ var energyTransferAnimation = new p5(function(sketch) {
 		//if (calculationFinished) {
 			// reset
 		//}
+	};
+
+	sketch.mousePressed = function() {
+		if (sketch.complex.mouseOver && !sketch.complex.beingDragged) {
+			sketch.complex.drag();
+		}
+	};
+
+	sketch.mouseReleased = function() {
+		if (sketch.complex.beingDragged) {
+			sketch.complex.drop();
+		}
 	};
 	
 }, 'energy-transfer-animation');
